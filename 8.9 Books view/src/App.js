7e032199b38b3d7a1647be7baf087e4ff93cd727
kabirolawore <horlaworey@gmail.com>
2024-01-24 03:22:33 +0000

    finished

---
+++ 8.9 Books view/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Authors from './components/Authors';
+import Books from './components/Books';
+import NewBook from './components/NewBook';
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS, ALL_BOOKS } from './queries';
+
+const App = () => {
+  const resultAuthors = useQuery(ALL_AUTHORS);
+  const resultBooks = useQuery(ALL_BOOKS);
+
+  const [page, setPage] = useState('authors');
+
+  if (resultAuthors.loading && resultBooks.loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+      <br />
+
+      <Authors
+        show={page === 'authors'}
+        authors={resultAuthors?.data?.allAuthors}
+      />
+
+      <Books show={page === 'books'} books={resultBooks?.data?.allBooks} />
+
+      <NewBook show={page === 'add'} />
+    </div>
+  );
+};
+
+export default App;
